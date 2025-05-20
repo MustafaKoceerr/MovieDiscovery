d08bd991d2c4f1d6669c2cd4c7fffd88b7c3660c
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-20 21:29:21 +0300

    Add Constants object for API and category definitions

--- app/src/main/java/com/example/moviediscovery/util/Constants.kt
+++ app/src/main/java/com/example/moviediscovery/util/Constants.kt
@@ -1,2 +1,15 @@
 package com.example.moviediscovery.util
 
+object Constants{
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val POSTER_SIZE = "w342"
+    const val BACKDROP_SIZE = "w780"
+    const val ORIGINAL_SIZE = "original"
+
+    // Categories for home screen
+    const val CATEGORY_NOW_PLAYING = "Now Playing"
+    const val CATEGORY_POPULAR = "Popular"
+    const val CATEGORY_TOP_RATED = "Top Rated"
+    const val CATEGORY_UPCOMING = "Upcoming"
+}
