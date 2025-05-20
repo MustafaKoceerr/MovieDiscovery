bd69ccd98898d50700cc3940979d0a8b3d28d630
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-20 19:26:35 +0300

    Home screen template added

--- app/src/main/java/com/example/moviediscovery/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/home/HomeScreen.kt
@@ -1,2 +1,8 @@
 package com.example.moviediscovery.presentation.home
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(onMovieClick: (Int) -> Unit, onSearchClick: () -> Unit) {
+    // TODO
+}
