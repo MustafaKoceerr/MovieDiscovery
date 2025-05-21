07f895ec203f913c348fff624725a50b60f7a672
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 11:45:02 +0300

    add SearchIntent to handle query changes, search action, and movie clicks

--- app/src/main/java/com/example/moviediscovery/presentation/search/SearchIntent.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/search/SearchIntent.kt
@@ -1,2 +1,9 @@
 package com.example.moviediscovery.presentation.search
 
+import retrofit2.http.Query
+
+sealed class SearchIntent {
+    data class QueryChanged(val query: String) : SearchIntent()
+    data class MovieClicked(val movieId: Int) : SearchIntent()
+    object SearchMovies : SearchIntent()
+}
