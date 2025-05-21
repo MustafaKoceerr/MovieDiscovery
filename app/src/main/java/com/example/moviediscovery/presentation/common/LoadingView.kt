a64965477a14fcf1b82f298c71ae55709a6d33f3
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 11:22:43 +0300

    Initial LoadingView composable and preview added for future styling

---
+++ app/src/main/java/com/example/moviediscovery/presentation/common/LoadingView.kt
@@ -0,0 +1,28 @@
+package com.example.moviediscovery.presentation.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.moviediscovery.presentation.theme.MovieDiscoveryTheme
+
+@Composable
+fun LoadingView(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingViewPreview() {
+    MovieDiscoveryTheme {
+        LoadingView()
+    }
+}
