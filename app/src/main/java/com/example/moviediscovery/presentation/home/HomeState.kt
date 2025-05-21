b8ef7bb5b5fee14abaac7d90d424e7ac0e85c6f9
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 08:48:49 +0300

    Add HomeState to hold movie lists and UI state

--- app/src/main/java/com/example/moviediscovery/presentation/home/HomeState.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/home/HomeState.kt
@@ -1,2 +1,13 @@
 package com.example.moviediscovery.presentation.home
 
+import com.example.moviediscovery.domain.model.Movie
+import java.lang.Error
+
+data class HomeState(
+    val nowPlayingMovies: List<Movie> = emptyList(),
+    val popularMovies: List<Movie> = emptyList(),
+    val topRatedMovies: List<Movie> = emptyList(),
+    val upcomingMovies: List<Movie> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String = ""
+)
