053559196df93a036df85eaf2493b57110d0c474
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 21:47:16 +0300

    color fixed with material theme

--- app/src/main/java/com/example/moviediscovery/presentation/detail/components/MovieDetailsContent.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/detail/components/MovieDetailsContent.kt
@@ -5,10 +5,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -36,6 +36,7 @@ fun MovieDetailsContent(
                     IconButton(onClick = onBackClick) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            tint =  MaterialTheme.colorScheme.onBackground,
                             contentDescription = stringResource(R.string.search_back)
                         )
                     }
