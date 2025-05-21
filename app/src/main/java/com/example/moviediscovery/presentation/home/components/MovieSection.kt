034431d8f635867fc9f5b1f3d0e2f2cf19dabeef
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 11:39:17 +0300

    move MovieItem from home to common module for reuse

--- app/src/main/java/com/example/moviediscovery/presentation/home/components/MovieSection.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/home/components/MovieSection.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.moviediscovery.domain.model.Movie
+import com.example.moviediscovery.presentation.common.MovieItem
 import com.example.moviediscovery.presentation.home.sampleMovies
 import com.example.moviediscovery.presentation.theme.MovieDiscoveryTheme
 
