a04d86810ee33b869478a9d9b58b1475d291d519
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 13:19:38 +0300

    optimized imports

--- app/src/main/java/com/example/moviediscovery/presentation/search/SearchIntent.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/search/SearchIntent.kt
@@ -1,7 +1,5 @@
 package com.example.moviediscovery.presentation.search
 
-import retrofit2.http.Query
-
 sealed class SearchIntent {
     data class QueryChanged(val query: String) : SearchIntent()
     data class MovieClicked(val movieId: Int) : SearchIntent()
--- app/src/main/java/com/example/moviediscovery/presentation/search/components/EmptySearchResults.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/search/components/EmptySearchResults.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.provider.FontsContractCompat.Columns
 import com.example.moviediscovery.presentation.theme.MovieDiscoveryTheme
 
 @Composable
