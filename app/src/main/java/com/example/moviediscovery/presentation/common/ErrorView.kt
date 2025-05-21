efee1446f5ce7a11bf14460c23a10087cb34783b
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 11:22:32 +0300

    Initial ErrorView composable and preview added for future styling

--- app/src/main/java/com/example/moviediscovery/presentation/common/ErrorView.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/common/ErrorView.kt
@@ -1,2 +1,55 @@
 package com.example.moviediscovery.presentation.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.moviediscovery.presentation.theme.MovieDiscoveryTheme
+
+@Composable
+fun ErrorView(
+    message: String,
+    onRetry: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            color = MaterialTheme.colorScheme.error,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(
+            onClick = onRetry,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Retry")
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorViewPreview() {
+    MovieDiscoveryTheme {
+        ErrorView(
+            message = "An error ocured",
+            onRetry = {},
+        )
+    }
+}
