1f05c96caeed81ed5c28a447546c33bb9db33403
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-21 08:49:31 +0300

    Add HomeIntent to handle movie loading and click actions

--- app/src/main/java/com/example/moviediscovery/presentation/home/HomeIntent.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/home/HomeIntent.kt
@@ -1,2 +1,6 @@
 package com.example.moviediscovery.presentation.home
 
+sealed class HomeIntent {
+    object LoadMovies : HomeIntent()
+    data class MovieClicked(val movieId: Int) : HomeIntent()
+}
