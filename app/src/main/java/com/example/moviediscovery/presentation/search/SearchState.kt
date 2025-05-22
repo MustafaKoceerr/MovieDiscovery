c80bb5c89d140968ab750e3f5adc3aea5b6ebdbf
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-22 18:03:11 +0300

    added pagination states to SearchState

--- app/src/main/java/com/example/moviediscovery/presentation/search/SearchState.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/search/SearchState.kt
@@ -6,5 +6,10 @@ data class SearchState(
     val query: String = "",
     val movies: List<Movie> = emptyList(),
     val isLoading: Boolean = false,
-    val error: String = ""
+    val error: String = "",
+
+    // Pagination specific
+    val currentPage: Int = 1,
+    val endReached: Boolean = false,
+    val isLoadingMore: Boolean = false
 )
