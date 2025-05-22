9d14475f790ac7f1de8e8a9d5d11a68d60b98f86
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-22 13:08:01 +0300

    updated MainActivity to handle recreation properly

--- app/src/main/java/com/example/moviediscovery/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviediscovery/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moviediscovery.presentation
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +11,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.moviediscovery.presentation.navigation.MovieNavGraph
 import com.example.moviediscovery.presentation.theme.MovieDiscoveryTheme
+import com.example.moviediscovery.util.LocaleHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        newBase?.let {
+            val context = LocaleHelper.applyLanguageToContext(it)
+            super.attachBaseContext(context)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
