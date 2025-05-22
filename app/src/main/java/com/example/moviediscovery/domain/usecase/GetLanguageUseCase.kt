984f0e37b19cca5405cc7366d8db18d488274f15
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-05-22 09:36:16 +0300

    add use case for reading language from DataStore

---
+++ app/src/main/java/com/example/moviediscovery/domain/usecase/GetLanguageUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.moviediscovery.domain.usecase
+
+import com.example.moviediscovery.domain.model.AppLanguage
+import com.example.moviediscovery.domain.repository.LanguageRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetLanguageUseCase @Inject constructor(
+    private val languageRepository: LanguageRepository
+) {
+    operator fun invoke(): Flow<AppLanguage> {
+        return languageRepository.getLanguage()
+    }
+
+}
